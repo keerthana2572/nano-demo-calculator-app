b45416814fc284a1bf873615d4d11cb3f589ef13
keerthana2572 <keerthana.d257@gmail.com>
2023-08-04 09:42:42 +0530

    modified

--- server.js
+++ server.js
@@ -10,20 +10,20 @@ app.use(express.json());
 const baseRouter = express.Router();
 
 baseRouter.get('/greeting', (req, res) => {
-    return res.send('hello user');
+    return res.send('Hello World!');
 });
 
 baseRouter.post('/add', (req, res) => {
     const {first,second}=req.body;
     const reslt=first+second;
-    res.json({ "result": reslt });
+    res.json({ result: reslt });
 });
 
 
 baseRouter.post('/subtract', (req, res) => {
     const {first,second}=req.body;
     const reslt=first-second;
-    res.json({ "result": reslt });
+    res.json({ result: reslt });
 });
 
 app.use(baseUrl, baseRouter);
